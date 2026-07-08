c231c8dbe81331dafb97904fc7df34372867794a
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2026-07-08 15:45:32 -0300

    first navbar and board style, with mixins, token and reset style

---
+++ frontend/src/app/ui/pages/board/board.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+
+@Component({
+  selector: 'app-board-page',
+  imports: [Navbar],
+  templateUrl: './board.html',
+  styleUrl: './board.scss',
+})
+export class BoardPage {}
